47b3c44eeb1d1237a214a4034964415efb3ac4f6
kanha <saikrishna.at2021@gmail.com>
2020-10-23 14:22:08 +0530

    New Weathers Added

--- DevOpsStarterSample/Controllers/WeatherForecastController.cs
+++ DevOpsStarterSample/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DevOpsStarterSample.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Rainy","Drizzling","jazz"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
